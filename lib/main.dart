c177b874a967f277affb09e40537de4ae6c3b8a3
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-10-21 15:51:45 +0600

    Car Parking App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:car_parking/Home_screen/HomePage.dart';
+import 'package:car_parking/Home_screen/HomePageOne.dart';
 import 'package:car_parking/welcome_screen/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePageOne(),
     );
   }
 }

627af5b450861d3bacd7816dcff9de43948a98f8
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-10-24 19:44:07 +0600

    Car Parking App

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:car_parking/authentication_screen/login_screen.dart';
 import 'package:car_parking/onboarding_screen/HomePageThree.dart';
 import 'package:car_parking/onboarding_screen/HomePageTwo.dart';
 import 'package:car_parking/onboarding_screen/HomePageOne.dart';
+import 'package:car_parking/reset_password.dart';
 import 'package:car_parking/welcome_screen/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: forgot_otp(),
+      home: reset_password(),
     );
   }
 }
---
+++ lib/reset_password.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class reset_password extends StatelessWidget {
+  const reset_password({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo.shade700,
+      ),
+      backgroundColor: Colors.indigo.shade700,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(height: 10,),
+              Form(
+                  child:Column(
+                    children: [
+                      Image.asset("assets/reset_password.png"),
+                      Text("Reset password", style: TextStyle(color: Colors.white, fontSize: 18,),),
+                      Text("Password must have 8 characters", style: TextStyle(color: Colors.white,),),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hint: Text("New Password", style: TextStyle(color: Colors.white),),
+                          suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                          border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Colors.grey,
+                                width: 2,
+                              )
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey, width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hint: Text("Confirm Password", style: TextStyle(color: Colors.white),),
+                          suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 2,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey, width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        height: 45,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.red,
+                        ),
+                        child: InkWell(
+                          child: Center(
+                            child: Text(
+                              "Update Password",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
